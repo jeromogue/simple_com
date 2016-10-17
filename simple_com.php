04c7843b75136250e7ee57e460f0297309a040f4
Jérôme Mogué <jerome.mogue@eemi.com>
2016-10-17 13:18:12 +0200

    ajout du fichier simple_com.php

---
+++ simple_com.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Simple_com
+ * @version 1.0
+ */
+/*
+Plugin Name: Simple com
+Description: This is a plugin that helps a lot to create a basic communication for e-commerce. It accompagnies the company in the creation of an article on Facebook in its facebook's page, and send e-mails to targets.
+Author: Jérôme Mogué
+Version: 1.0
+License: GPL2
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
